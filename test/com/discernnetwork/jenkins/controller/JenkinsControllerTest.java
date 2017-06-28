5deaa1d19ce8bde1e901cee3e86d92e0d848094c
devi <devi.sambandhan@treselle.com>
2017-06-28 21:35:34 +0530

    Added batch for deploy

--- test/com/discernnetwork/jenkins/controller/JenkinsControllerTest.java
+++ test/com/discernnetwork/jenkins/controller/JenkinsControllerTest.java
@@ -18,7 +18,7 @@ public class JenkinsControllerTest {
     @Test
     public void testPrintMessage() {
         MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
-        mockHttpServletRequest.addParameter("message", "hello");
+        mockHttpServletRequest.addParameter("message", "");
         Object message = this.jenkinsController.printGivenMessage(mockHttpServletRequest);
         if(message != null) {
 			Assert.assertEquals("Hi!!!Jenkins!!!",message);

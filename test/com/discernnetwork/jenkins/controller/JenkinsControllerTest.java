eaa1602197800eaf38faf3e50663f21866af65bf
devi <devi.sambandhan@treselle.com>
2017-06-28 16:26:22 +0530

    Added Junit test failure

--- test/com/discernnetwork/jenkins/controller/JenkinsControllerTest.java
+++ test/com/discernnetwork/jenkins/controller/JenkinsControllerTest.java
@@ -1,6 +1,7 @@
 package com.discernnetwork.jenkins.controller;
 
 import org.junit.Test;
+import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletRequest;

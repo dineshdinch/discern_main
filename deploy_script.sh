d1c49f92ae96de19157ee1588cdf6042ffc4feec
devi <devi.sambandhan@treselle.com>
2017-06-28 18:51:42 +0530

    changed the config for Tomcat server

--- deploy_script.sh
+++ deploy_script.sh
@@ -3,9 +3,9 @@
 echo "Removing old war files"
 cd D:/apache-tomcat-7.0.69/webapps
 
-rm -r pqs
+rm -r jenkins_main_project
 
-rm pqs.war
+rm jenkins_main_project.war
 
 echo "Copying the war file into Tomcat"
 cp E:/Jenkins/main/discern_main/dist/jenkins_main_project.war D:/apache-tomcat-7.0.69/webapps

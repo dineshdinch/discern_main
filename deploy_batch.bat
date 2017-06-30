2fa86bbd2dc99ca066c6903460ac08c2870cdf82
devi <devi.sambandhan@treselle.com>
2017-06-30 15:44:31 +0530

    changed the execution dir

--- deploy_batch.bat
+++ deploy_batch.bat
@@ -8,6 +8,8 @@ rmdir jenkins_main_project
 
 xcopy E:\Jenkins\main\discern_main\dist\jenkins_main_project.war D:\apache-tomcat-7.0.69\webapps
 
+E:
+
 cd E:\Jenkins\main\discern_main
 
 tomcat_script.bat
